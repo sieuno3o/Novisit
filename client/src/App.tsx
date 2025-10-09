02029e3d36e82249bd6c81fa8e0db4473b53772d
김시은 <tldms3241@gmail.com>
2025-10-10 02:12:01 +0900

    refactor : 라우팅 변경

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./auth";
 import RequireAuth from "./routes/RequireAuth";
+import Layout from "./components/Layout";
 import MainPage from "./pages/MainPage";
 import LoginPage from "./pages/LoginPage";
+import KakaoFinishPage from "./pages/KakaoFinishPage";
 import NoticePage from "./pages/NoticePage";
 import MyPage from "./pages/MyPage";
-import KakaoFinishPage from "./pages/KakaoFinishPage";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/oauth/kakao" element={<KakaoFinishPage />} />
-      <Route element={<RequireAuth />}>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/notice" element={<NoticePage />} />
-        <Route path="/mypage" element={<MyPage />} />
-      </Route>
-    </Routes>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route element={<Layout />}>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/oauth/kakao" element={<KakaoFinishPage />} />
+            <Route element={<RequireAuth />}>
+              <Route path="/notice" element={<NoticePage />} />
+              <Route path="/mypage" element={<MyPage />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
