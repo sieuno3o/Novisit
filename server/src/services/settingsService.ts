b132db0077f812804202aebb735a3c5b74541c54
PKJYJ <p134812024@gmail.com>
2025-10-29 02:47:47 +0900

    feat: /settings에 channel, date 추가

--- server/src/services/settingsService.ts
+++ server/src/services/settingsService.ts
@@ -3,11 +3,11 @@ import { createSetting, getSettings, updateSetting, deleteSetting } from "../rep
 
 // 알림 설정 생성
 export async function createUserSetting(userId: string, data: any) {
-  const { domain_id, name, filter_keywords, url_list } = data;
+  const { domain_id, name, filter_keywords, url_list, channel } = data;
 
   // 필수 값 확인
-  if (!domain_id || !name) {
-    throw new Error("domain_id와 name은 필수입니다.");
+  if (!domain_id || !name || !channel ) {
+    throw new Error("domain_id, name, channel은 필수입니다.");
   }
 
   // DB에 저장할 데이터 구성
@@ -17,6 +17,7 @@ export async function createUserSetting(userId: string, data: any) {
     name,
     url_list: url_list || [],
     filter_keywords: filter_keywords || [],
+    channel,
     messages: [],
   };
 
